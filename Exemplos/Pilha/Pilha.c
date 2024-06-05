40f6a9dbd4e310bab5f3203f8a0f5f450f2dd69f
Mateus <mateus.olvr2002@gmail.com>
2024-06-05 16:11:42 -0300

    Criação do arquivo 'Pilha.c'

---
+++ Exemplos/Pilha/Pilha.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Prato{
+	char cor;
+	struct Prato *prox;
+};
+
+typedef struct Prato prato;
+
+prato *pilha;
+
+void add(char cor){
+	prato *p = (prato*) malloc(sizeof(prato));
+	p->cor = cor;
+	p->prox = pilha; // Aponta o novo elemento para o primeiro elemento da fila
+	pilha = p; // O novo elemento se torna o primeiro
+}
+
+void rem(){
+	if(pilha == NULL){
+		printf("Pilha vazia!");
+	} else pilha = pilha->prox;
+}
+
+void imprime(prato *p){
+	if(p != NULL){
+		printf("Cor do prato: %c\n", p->cor);
+		imprime(p->prox);
+	}
+}
+
+int main(){
+	
+	add('b');
+	add('g');
+	add('r');
+	add('p');
+	
+	prato *pilhaAux = pilha;
+	imprime(pilhaAux);
+	
+	rem(pilha);
+	
+	printf("-----------------------\n");
+	pilhaAux = pilha;
+	imprime(pilhaAux);
+	
+	return 0;
+}
